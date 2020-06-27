e334a856d2d7d44e0f6f26879b07b9d89876aa38
Frank Lai <frank810618@gmail.com>
2020-06-27 09:47:15 +0800

    add navigate feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Recipe';
+  feature: string;
+
+  onNavigate(feature: string) {
+    this.feature = feature;
+  }
 }
