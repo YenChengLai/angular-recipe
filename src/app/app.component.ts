b9b4d238762d74764fee3fb666e6f1d00d50db4e
Frank Lai <frank810618@gmail.com>
2020-07-03 11:29:28 +0800

    set navigation with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular-Recipe';
   feature: string;
-
-  onNavigate(feature: string) {
-    this.feature = feature;
-  }
 }
