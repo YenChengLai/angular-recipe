94eccfda1dd0b25c5353633c5d7ef6518d63512d
Frank Lai <frank810618@gmail.com>
2020-07-23 09:54:12 +0800

    add fetch datat feature

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataStorageSvc: DataStorageService) { }
 
   ngOnInit() {
   }
 
+  onSaveDate() {
+    this.dataStorageSvc.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageSvc.fetchRecipes();
+  }
 }
