9eac52f0512f8e5b3edd84b83d5a1335a3344396
Frank Lai <frank810618@gmail.com>
2020-07-27 22:43:43 +0800

    adjust UI with authentication states

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor(private dataStorageSvc: DataStorageService) { }
+  private userSubscription: Subscription;
+  isAuthenticated = false;
+
+  constructor(private dataStorageSvc: DataStorageService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
   }
 
   onSaveDate() {
@@ -19,5 +27,9 @@ export class HeaderComponent implements OnInit {
 
   onFetchData() {
     this.dataStorageSvc.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 }
