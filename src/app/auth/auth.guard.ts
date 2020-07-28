1587acf91e12e84399b6b1f59f1b8bbe4b0a6b11
Frank Lai <frank810618@gmail.com>
2020-07-28 13:15:58 +0800

    add guards to prevent unauthorized visits

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Promise<boolean> | Observable<boolean> {
-    return;
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
+    return this.authService.user.pipe(take(1), map(user => {
+      const isAuth = !!user;
+      if (isAuth) {
+        return true;
+      }
+      return this.router.createUrlTree(['/auth']);
+    }));
   }
 }
